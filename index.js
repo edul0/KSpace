2cc3b39a4e2305f6b7aebd6ce7d32c4fa64f5077
EduardoL <eduardolesse0@gmail.com>
2026-03-13 06:48:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,11 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<h1 class="text-3xl font-bold p-4">Project_Kanban</h1>
+<hr>
 
-    <div id="kanban-board" class="board-container">
-        </div>
+<div id="kanban-board" class="board-container"></div>
+
 
     <script src="script.js"></script>
     <script>
